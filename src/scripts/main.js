f80819a2676d51d06f377aa285be98f909ba4c35
Yana <anazigaluk@gmail.com>
2024-12-11 17:45:50 +0200

    modify list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const items = document.querySelectorAll('li');
+const tree = document.querySelector('.tree');
+
+for (const item of items) {
+  const span = document.createElement('span');
+
+  item.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.onclick = (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const childrenContainer = e.target.parentNode.querySelector('ul');
+
+  childrenContainer.hidden = !childrenContainer.hidden;
+};
